079646def5d4a3f850f5163c13689a306b97ead5
chenyuxuan <2176807108@qq.com>
2022-09-17 17:20:39 +0800

    Create 1069.cpp

---
+++ cses/1069.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	string s;
+	int maxx=INT_MIN,sum=1;
+	cin>>s;
+	for(int i=0;i<s.size();i++)
+	{
+		if(s[i]!=s[i+1])
+		{
+			maxx=max(maxx,sum); 
+			sum=1;
+		}
+		else
+		{
+			sum++;
+		}
+	}
+	cout<<maxx<<endl;
+	return 0;
+}
+
